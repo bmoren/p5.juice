4fdb28e6a3f568869913281e3fa14e7fc11c14e0
bmoren <ben@benmoren.com>
2022-09-12 20:42:36 -0500

    add particles, screen shake, and soundFX

--- p5.juice.js
+++ p5.juice.js
@@ -5,12 +5,35 @@ class Juice{
         this.tweenTarget = 0
         this.tweenSpeed = 0.1
         this.incTweenStart = false
-        
-        // mouseSpeedJelly Settings
+
+        // shake
+        this.shakeTween = 0
+        this.shakeCount = 0
+        this.shakeDirection = 1
+
+        //note SFX
+        this.monoSynth = new p5.MonoSynth();
+        // userStartAudio();
+
+        //particles
+        this.particleSettings = false;
+        this.particleArray = []
+        this.allParticlesDied = false;
+
+        this.particlesLifespan = 0
+        this.particlesX = 0 
+        this.particlesY = 0
+
+
+        //screen Shake
+        this.screenShakeX = 0
+        this.screenShakeY = 0
+        this.screenShakeDuration = 0
 
         
     }
 
+    //speed is optional
     tween(start,target,speed){
         this.tweenStart = start
         this.tweenTarget = target 
@@ -30,7 +53,7 @@ class Juice{
         return this.incTweenStart += (this.tweenTarget - this.incTweenStart) * this.tweenSpeed 
     }
 
-
+    //Mouse Speed Section
 
     mouseSpeedX(){
         return Math.abs(mouseX - pmouseX);
@@ -40,28 +63,65 @@ class Juice{
         return Math.abs(mouseY - pmouseY);
     }
 
-    mouseSpeedJelly(){
-        //mouseSpeed with easing
-
+    mouseSpeed(){
+        return this.mouseSpeedX() + this.mouseSpeedY()
     }
 
-    
-
-    shake(){
-        //a small wiggle animation
-
-    }
-
-    sfx(audioFile){
+    //Audio File SFX
+    sfx(audioFile, startTime){
         //audio file based sound effect
+
+        audioFile.play(startTime)
     }
 
-    ssfx(){
+    //single note SFX
+    synthNote(note,duration){
         //synth based sound effect
+        this.monoSynth.play(note, 1, 0, duration);
+
     }
 
-    particles(){
-        // a small particle engine with a plume of ellipses or rectangles
+    particles(x,y, size, speed, lifespan, r,g,b,a){
+        // a small particle engine with a plume of ellipses 
+
+        //save these so we can reset
+        this.particlesLifespan = lifespan
+        this.particlesX = x
+        this.particlesY = y
+
+        //generate particles if it's the first time the function runs.
+        if(this.particleSettings == false && this.particleArray < 10){
+            for(let i = 0 ; i < 10; i++){
+                let p = new particle(x,y,size,speed,lifespan,r,g,b,a)
+                this.particleArray.push(p)
+            }
+        }
+        //make sure the particles exist
+        if(this.particleArray != 0){
+            for(let i = 0 ; i < this.particleArray.length; i++){
+
+                if(this.particleArray[i].lifespan > 0){
+                    this.particleArray[i].render();
+                }
+            }
+        }
+
+    }
+
+    particlesReset(){
+
+                //make sure the particles exist
+                if(this.particleArray != 0){
+                    for(let i = 0 ; i < this.particleArray.length; i++){
+        
+                        this.particleArray[i].lifespan =  this.particlesLifespan
+                        this.particleArray[i].x =  this.particlesX
+                        this.particleArray[i].y =  this.particlesY
+
+
+                    }
+                }
+
     }
 
     pathHistory(){
@@ -69,18 +129,53 @@ class Juice{
         //second attribute specifys the length? or maybe this is just a variable?
     }
 
-    screenShake(){
+    screenShake(shakeAmount){
         // shake the entire canvas
         //do this with p5.dom instead of moving everything on the cavas!
+
+        this.screenShakeX += random(-shakeAmount, shakeAmount)
+        this.screenShakeX = constrain(this.screenShakeX,-shakeAmount, shakeAmount)
+
+        this.screenShakeY += random(-shakeAmount, shakeAmount)
+        this.screenShakeY = constrain(this.screenShakeY,-shakeAmount, shakeAmount)
+
+
+        select('canvas').position(this.screenShakeX, this.screenShakeY);
+
+        console.log(shakeAmount, this.screenShakeX, this.screenShakeY)
+
     }
 
-    addFace(){
-        // maybe? add a face to a specificed location, have the eyes follow a specified point.
+
+
+}
+
+
+
+class particle{
+    constructor(x,y,size, speed, lifespan,r,g,b,a){
+        this.x = x
+        this.y = y
+        this.speed = speed
+        this.sz = random(size/2, size)
+        this.lifespan = random(lifespan/2, lifespan)
+        this.r = r
+        this.g = g 
+        this.b = b
+        this.a = a
+
     }
 
-
-
-
-
-
+    render(){
+        //brownian motion
+        this.x += random(-this.speed, this.speed)
+        this.y += random(-this.speed, this.speed)
+        this.lifespan --
+        
+        push()
+            noStroke()
+            fill(this.r, this.g, this.b, this.a)
+             ellipse(this.x,this.y,this.sz)
+        pop()
+    }
 }
--- p5/sketch.js
+++ p5/sketch.js
@@ -1,28 +1,71 @@
 
-let tween
+// // tween
 
-let xpos = 100
+// let tween
 
+// let xpos = 100
+
+
+// function setup() {
+//   // put setup code here
+//   createCanvas(600,600)
+
+//   tween = new Juice()
+
+// }
+
+// function draw() {
+//   // put drawing code here
+
+//   ellipse( xpos, height/2,20,20 )
+
+//   if(mouseIsPressed == true){
+//     xpos = tween.tween(100,500)
+//   }
+
+//   if(keyIsPressed == true){
+//     xpos = tween.tween(500,0)
+
+//   }
+
+
+
+
+//   ellipse()
+// }
+
+
+// Shake
+
+
+let juice
+let wiggle = 0
 
 function setup() {
   // put setup code here
   createCanvas(600,600)
 
-  tween = new Juice()
+  juice = new Juice()
 
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+  background(0);
 
-  ellipse( xpos, height/2,20,20 )
+  if(mouseIsPressed === true){
+    juice.particles(200,100,10,5,10,255,50,100,255)
 
-  if(mouseIsPressed == true){
-    xpos = tween.tween(100,500)
+
+    juice.screenShake(10)
+ 
   }
 
-  if(keyIsPressed == true){
-    xpos = tween.tween(500,0)
-
+  if(mouseIsPressed === false){
+    juice.particlesReset()
   }
+
+
+  ellipse(width/2 + wiggle,height/2,20,20)
+
+
 }
